f0be56894a7057dba34b30b4ecb45a084be308ca
yonas pro <yonasbe999@gmail.com>
2024-03-17 10:37:50 -0700

    2nd part completed

---
+++ frontend/src/Pages/home/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Sidebar } from "../../Components/sidebar/Sidebar";
+import { MessagesContainer } from "../../Components/messages/MessagesContainer";
+
+export const Home = () => {
+  return (
+    <div className="flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
+      <Sidebar />
+      <MessagesContainer />
+      <></>
+    </div>
+  );
+};
